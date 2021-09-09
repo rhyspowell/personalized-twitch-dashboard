efc33b64dfdfc8d65e04970e3b4f575c3eca65f0
Rhys Powell <rhys@rhyspowell.com>
2021-09-09 12:59:57 +0000

    Add streamerGrid component

--- components/StreamerGrid/StreamerGrid.js
+++ components/StreamerGrid/StreamerGrid.js
@@ -2,17 +2,24 @@ import React from 'react'
 import Image from 'next/image'
 import styles from '../../styles/StreamerGrid.module.css'
 
+const StreamerGrid = ({ channels }) => {
+  return (
+    <div>
+      <p>Hello Streamer Grid</p>
+    </div>
+  )
+}
 //const StreamerGrid = ({ channels })
 
-const renderGridItem = channel => (
-  <div key={channel.id} className={styles.gridItem}>
-    <Image Layout="fill" src={channel.thumbnail_url} />
-    <div classname={styles.gridItemContent}>
-      <p>{channel.display_name}</p>
-      {channel.is_live && <p> Live Now!</p>}
-      {!channel.is_live && <p> Offline</p>}
-    </div>
-  </div>
+//const renderGridItem = channel => (
+//  <div key={channel.id} className={styles.gridItem}>
+//    <Image Layout="fill" src={channel.thumbnail_url} />
+//    <div classname={styles.gridItemContent}>
+//      <p>{channel.display_name}</p>
+//      {channel.is_live && <p> Live Now!</p>}
+//      {!channel.is_live && <p> Offline</p>}
+//    </div>
+//  </div>
 
 //  return (
 //    <div>
@@ -20,7 +27,7 @@ const renderGridItem = channel => (
 //      {channels.map(renderGridItem)}
 //    </div>
 //  )
-)
+//)
 
 
 
